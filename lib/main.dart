1d129bb50d10313c3379579214ec3ab428de1997
Joel <86185696+AnJasid@users.noreply.github.com>
2025-05-15 01:34:13 +0800

    add new file for login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:app_to_do_list/screens/todo_list_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:app_to_do_list/screens/login_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: LoginScreen(),
     );
   }
 }
