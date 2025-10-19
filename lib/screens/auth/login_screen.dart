c22df40fd828f072115dd687de0c51017ea2ef95
Joel <86185696+AnJasid@users.noreply.github.com>
2025-10-19 20:18:50 +0800

    refactor: improve login form UI and fix layout issues

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,148 +1,121 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:app_to_do_list/screens/auth/signup_screen.dart';
+import 'package:flutter/gestures.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
-  void _goToSignUpScreen(context) {
-    Navigator.of(context).push(
-      MaterialPageRoute(builder: (ctx) => SignupScreen()),
-    );
-  }
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool _isPasswordVisible = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-      ),
-      backgroundColor: Colors.blue[800],
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              child: Text(
-                'Login',
-                style: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.w700,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15.0),
-              child: Text(
-                'Enter a beautiful world',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30),
-                    topRight: Radius.circular(30),
-                  ),
-                ),
-                child: Form(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 15),
-                        child: TextFormField(
-                          decoration: InputDecoration(
-                            labelText: 'Your Email',
-                            hintText: 'example@email.com',
-                            border: OutlineInputBorder(),
-                            prefixIcon: Icon(Icons.email_outlined),
-                            filled: true,
-                            fillColor: Colors.grey[200],
-                          ),
-                        ),
+      resizeToAvoidBottomInset: true,
+      body: SafeArea(
+        child: Container(
+          color: Colors.white,
+          child: Center(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Image.asset(
+                      'assets/images/login.png',
+                      width: 230,
+                    ),
+                    SizedBox(height: 30),
+                    Text(
+                      'Login',
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.w400),
+                    ),
+                    SizedBox(height: 30),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Your Email',
+                        hintText: 'example@email.com',
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.email_outlined),
+                        filled: true,
+                        fillColor: Colors.white,
                       ),
-                      SizedBox(height: 30),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 15),
-                        child: TextFormField(
-                          decoration: InputDecoration(
-                            labelText: 'Password',
-                            border: OutlineInputBorder(),
-                            prefixIcon: Icon(Icons.lock_outline),
-                            filled: true,
-                            fillColor: Colors.grey[200],
+                    ),
+                    SizedBox(height: 50),
+                    TextFormField(
+                      obscureText: _isPasswordVisible,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.lock_outline),
+                        filled: true,
+                        fillColor: Colors.white,
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _isPasswordVisible
+                                ? Icons.visibility
+                                : Icons.visibility_off,
                           ),
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 15),
-                        child: GestureDetector(
-                          onTap: () {
-                            print('Hello');
+                          onPressed: () {
+                            setState(() {
+                              _isPasswordVisible = !_isPasswordVisible;
+                            });
                           },
-                          child: Text(
-                            'forget password',
-                            style: TextStyle(color: Colors.blue),
-                          ),
                         ),
                       ),
-                      SizedBox(height: 30),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 15),
-                        child: SizedBox(
-                          width: double.infinity,
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.blue[800],
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(4),
-                              ),
+                    ),
+                    SizedBox(height: 20),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        'forget password',
+                        style: TextStyle(color: Colors.blue),
+                      ),
+                    ),
+                    SizedBox(height: 40),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blue[800],
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: Text(
+                          'Login',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    RichText(
+                      text: TextSpan(
+                        style: TextStyle(color: Colors.black),
+                        children: <TextSpan>[
+                          TextSpan(text: "Don't Have An Account? "),
+                          TextSpan(
+                            text: 'Sign Up Now',
+                            style: TextStyle(
+                              color: Colors.blue,
                             ),
-                            onPressed: () {},
-                            child: Text(
-                              'LOGIN',
-                              style: TextStyle(
-                                color: Colors.white,
-                              ),
-                            ),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () => () {},
                           ),
-                        ),
+                        ],
                       ),
-                      SizedBox(height: 70),
-                      Center(
-                        child: RichText(
-                          text: TextSpan(
-                            style: TextStyle(color: Colors.black),
-                            children: <TextSpan>[
-                              TextSpan(text: "Don't Have An Account? "),
-                              TextSpan(
-                                text: 'Sign Up Now',
-                                style: TextStyle(
-                                  color: Colors.blue,
-                                ),
-                                recognizer: TapGestureRecognizer()
-                                  ..onTap = () => _goToSignUpScreen(context),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             ),
-          ],
+          ),
         ),
       ),
     );
